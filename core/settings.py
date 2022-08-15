9a0df6771c0a4c244ed8dad2b64f03c1e2da8485
marshDesigns <72623348+marshDesigns@users.noreply.github.com>
2022-08-15 01:24:24 -0700

    host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--gica=-dbq)g@h%_m2va%iewnt7!$q_q=*n=l&kj3(=qi#&bbp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','stocks-django.herokuapp.com']
 
 
 # Application definition
