4c26db7039f09779262db1df98190acad7e9b6b5
marshDesigns <72623348+marshDesigns@users.noreply.github.com>
2022-08-15 01:17:19 -0700

    commit2

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.conf.urls.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
